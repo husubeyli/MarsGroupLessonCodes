bb46883672b8848f4f05e869032f66cdf8f6dd50
idris <idris.sabanli@gmail.com>
2020-11-19 11:25:28 +0400

    bug fixed

--- web_scraping_csv/main.py
+++ web_scraping_csv/main.py
@@ -10,10 +10,10 @@ with open('dayaz.html', 'w') as f:
 
 soup = BeautifulSoup(response.text, "html.parser" )
 
-parent= soup.findAll("div", {'class': 'news-text'})
+parent= soup.findAll("div", {'class': 'news-text'})[0]
 news_paragraph = parent.findChildren("p" , recursive=False)
 
-
+# image, title, published_date, published_time, link, content
 
 print(news_paragraph[0].text)
 
