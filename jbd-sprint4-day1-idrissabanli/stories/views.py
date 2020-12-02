8f5bb41af3bec993d8b305cce3b50214292a8867
idris <idris.sabanli@gmail.com>
2020-12-02 11:47:56 +0400

    django filter and tags template inheritance and custome filters and tags

--- jbd-sprint4-day1-idrissabanli/stories/views.py
+++ jbd-sprint4-day1-idrissabanli/stories/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import Http404
+from datetime import datetime
 
 users = [
     {
         'id': 1,
-        'name': 'Kenan'
+        'name': 'Kenan salam',
+        'desc': '''<h1>Lorem ipsum, dolor sit amet 
+        consectetur adipisicing elit. Numquam error 
+        aperiam sint laboriosam deserunt impedit quibusdam aut 
+        consequatur pariatur autem exercitationem, 
+        fuga iure, omnis quo in 
+        ipsam nihil perferendis ipsum.</h1>''',
+        'birth_day': datetime.now()
     },
     {
         'id': 2,
@@ -17,6 +25,7 @@ users = [
 ]
 
 def test(request):
+    
     context = {
         'user_list': users
     }
