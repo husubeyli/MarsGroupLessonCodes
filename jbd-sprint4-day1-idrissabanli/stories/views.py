a27beb056aca7bda8993bbb63db086cea3494b65
idris <idris.sabanli@gmail.com>
2020-12-07 11:41:03 +0400

    django querysets filters

--- jbd-sprint4-day1-idrissabanli/stories/views.py
+++ jbd-sprint4-day1-idrissabanli/stories/views.py
@@ -1,14 +1,33 @@
+import math
 from django.shortcuts import render
 from django.http import Http404
 from datetime import datetime
+from django.db.models import Q
 
 from stories.models import Author
 
 
 def test(request):
     users = Author.objects.all()
+    searched_name = request.GET.get('search')
+    order_by = request.GET.get('order')
+    per_count = 3
+    print(searched_name)
+    page = int(request.GET.get('page', 1))
+    if searched_name:
+        users = users.filter(Q(first_name__icontains=searched_name) 
+        | Q(last_name__icontains=searched_name))
+    if order_by:
+        users = users.order_by(order_by)
+
+    page_count = math.ceil(users.count()/per_count)
+    users = users[(page-1)*per_count:page*per_count]
+    page_range = range(1, page_count+1)
     context = {
-        'user_list': users
+        'user_list': users,
+        'current_page': page,
+        'searched_name': searched_name if searched_name else '',
+        'page_range': page_range
     }
     return render(request, 'users.html', context)
 
@@ -16,7 +35,7 @@ def test(request):
 def user_detail(request, user_id):
     # if user_id > len(users):
     #     raise Http404
-    user = users[user_id-1]
+    user = []
     # print(request.GET)
     # user = request.GET.get('selected_user', 'Secilmis User yoxdur')
     context = {
